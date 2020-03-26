ff9331e677a4116cdeb24ab09b7895085f8fa756
MattisTrees <tuj68316@temple.edu>
2020-03-26 23:29:54 +0000

    Create new links to site and add functionality for user login in SafeHomeDevice/views.py

--- SafeHomeDatabase/views.py
+++ SafeHomeDatabase/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
-
+from SafeHomeDatabase.models import *
 from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
 	return HttpResponse("Hello, world. You're at the Database index.")
+
+def signin(request):
+	# Retrieve the GET request value for 'email'
+	inputEmail = request.GET.get('email')
+	# Retrieve the GET request value for 'password'
+	inputPassword = request.GET.get('password')
+	# Retrieve the all of the models for 'Users' from the database
+	try:
+		user = Users.objects.get(email=inputEmail).email
+		print(user)
+	except:
+		return HttpResponse("User not found")
+	if inputEmail == user:
+		return HttpResponse("User has been found!")
+	else:
+		return HttpResponse("Receiving this response means there is a problem with the return of 'Users.objects.get(email=inputEmail).email")
+	return HttpResponse("Email: " + inputEmail + " Pass: " + inputPassword)
+
